17238c6732ef84fe9a45069134f2ade13c9324b8
Biknac <91196478+Biknac@users.noreply.github.com>
2023-02-14 17:04:04 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.oc-slider').slick({
+    infinite: true,
+    swipe: false,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    variableWidth: true
+  });
+});

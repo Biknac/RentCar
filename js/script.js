7c8df85d1d3708691dbf6770347e5f6cd5bd728f
Biknac <91196478+Biknac@users.noreply.github.com>
2023-02-14 23:36:09 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,25 @@ $(document).ready(function() {
     variableWidth: true
   });
 });
+
+
+window.onresize = start;
+function start(){
+  let iw = window.innerWidth;
+  let ih = window.innerHeight;
+
+  console.log("Window size:");
+  console.log(iw);
+  console.log(ih);
+
+  if (iw >= 1280 && ih >= 720  ){
+    $('.mobile').removeClass('_show');
+  }
+  
+  if (iw < 1280 || ih < 720 ){
+    $('.mobile').addClass('_show');
+  }
+
+}
+
+start();
